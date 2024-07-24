6642272d2e2c98dbd22807422f33120ad7fc5e0f
Praveen Kumar <praveen9166050@gmail.com>
2024-07-24 23:35:02 +0530

    Worked on VideoDetail component

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,22 +1,25 @@
 import { Box, Stack } from '@mui/material'
 import { VideoCard, ChannelCard } from './';
 
-function Videos({videos}) {
-  return (
-    <Stack direction='row' flexWrap='wrap' justifyContent='start' alignItems='start' gap={2}>
-        {videos.map(item => {
-            if (item.id.playlistId) {
-                return;
-            }
-            return (
-                <Box key={item.id.videoId || item.id.channelId}>
-                    {item.id.videoId && <VideoCard video={item} />}
-                    {item.id.channelId && <ChannelCard channelDetail={item} />}
-                </Box>
-            );
-        })}
-    </Stack>
-  )
+function Videos({videos, direction}) {
+    if (!videos?.length) {
+        return 'Loading...'
+    }
+    return (
+        <Stack direction={direction || 'row'} flexWrap='wrap' justifyContent='start' alignItems='start' gap={2}>
+            {videos.map(item => {
+                if (item.id.playlistId) {
+                    return;
+                }
+                return (
+                    <Box key={item.id.videoId || item.id.channelId}>
+                        {item.id.videoId && <VideoCard video={item} />}
+                        {item.id.channelId && <ChannelCard channelDetail={item} />}
+                    </Box>
+                );
+            })}
+        </Stack>
+    )
 }
 
 export default Videos
